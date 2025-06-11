d5007e132f176304ca0a3d8a7564313fe0aec2c1
Phen-Reaksmey <reaksmeyphen777@gmail.com>
2025-06-11 20:26:31 +0700

    first commit

--- database/migrations/2025_06_11_122724_create_products_table.php
+++ database/migrations/2025_06_11_122724_create_products_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,10 +15,11 @@ return new class extends Migration
             $table->string("name")->nullable();
             $table->double("cost", 10, 2);
             $table->double("price", 10, 2);
-            $table->foreignId("categories_id")->constrained()->onDelete("cascade");
-            $table->foreignId("brands_id")->constrained()->onDelete("cascade");
+            $table->foreignId("category_id")->constrained()->onDelete("cascade");
+            $table->foreignId("brand_id")->constrained()->onDelete("cascade");
             $table->timestamps();
         });
+
     }
 
     /**
