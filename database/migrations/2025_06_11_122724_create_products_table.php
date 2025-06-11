9190f13f944befed690c83321495139e738db260
Phen-Reaksmey <reaksmeyphen777@gmail.com>
2025-06-11 20:31:40 +0700

    first commit

--- database/migrations/2025_06_11_122724_create_products_table.php
+++ database/migrations/2025_06_11_122724_create_products_table.php
@@ -15,7 +15,9 @@ return new class extends Migration {
             $table->string("name")->nullable();
             $table->double("cost", 10, 2);
             $table->double("price", 10, 2);
-            $table->foreignId("category_id")->constrained()->onDelete("cascade");
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
             $table->foreignId("brand_id")->constrained()->onDelete("cascade");
             $table->timestamps();
         });
